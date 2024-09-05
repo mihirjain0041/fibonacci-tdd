f51570e96be0cb91542babce1397aecc73db71bf
mihirjain0041 <92995354+mihirjain0041@users.noreply.github.com>
2024-09-06 01:13:46 +0530

    Implemented full fibonacci logic to pass all tests

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,6 +1,9 @@
 function fibonacci(n) {
-    if (n === 1) return [0];
-    if (n === 2) return [0, 1];
+    const series = [0, 1];
+    for (let i = 2; i < n; i++) {
+        series.push(series[i - 1] + series[i - 2]);
+    }
+    return series.slice(0, n);
 }
 
 module.exports = fibonacci;
