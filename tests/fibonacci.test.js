1b0b605f9ef88a1a6535693981055e1af254edac
mihirjain0041 <92995354+mihirjain0041@users.noreply.github.com>
2024-09-06 00:55:21 +0530

    Added failing test for fibonacci function with input 2

--- tests/fibonacci.test.js
+++ tests/fibonacci.test.js
@@ -3,3 +3,7 @@ const fibonacci = require('../src/fibonacci');
 test('returns [0] for input 1', () => {
     expect(fibonacci(1)).toEqual([0]);
 });
+
+test('returns [0, 1] for input 2', () => {
+    expect(fibonacci(2)).toEqual([0, 1]);
+});
