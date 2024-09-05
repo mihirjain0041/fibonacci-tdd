acae2b7abddd89271fb82bc175de16ebf593d24e
mihirjain0041 <92995354+mihirjain0041@users.noreply.github.com>
2024-09-06 01:12:17 +0530

    Added failing tests for fibonacci function with input 3 and 5

--- tests/fibonacci.test.js
+++ tests/fibonacci.test.js
@@ -7,3 +7,11 @@ test('returns [0] for input 1', () => {
 test('returns [0, 1] for input 2', () => {
     expect(fibonacci(2)).toEqual([0, 1]);
 });
+
+test('returns [0, 1, 1] for input 3', () => {
+    expect(fibonacci(3)).toEqual([0, 1, 1]);
+});
+
+test('returns [0, 1, 1, 2, 3] for input 5', () => {
+    expect(fibonacci(5)).toEqual([0, 1, 1, 2, 3]);
+});
